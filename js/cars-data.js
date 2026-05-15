cfc08f104a66315e1f363ae3d7f3fc281d12560a
Ivan Lednev <feyrune33@yandex.ru>
2026-05-15 23:43:29 +0300

    feat: add timer layout

--- js/cars-data.js
+++ js/cars-data.js
@@ -12,9 +12,9 @@ const cars = [
       'Допоборудование на 100 000р в подарок',
     ],
     colors: [
-      { name: 'Чёрный неметаллик', hex: '#1a1a1a', img: 'assets/img/cars/m6/black.png' },
-      { name: 'Серый неметаллик',  hex: '#c0c0c0', img: 'assets/img/cars/m6/silver.png' },
-      { name: 'Белый неметаллик',  hex: '#f0f0f0', img: 'assets/img/cars/m6/white.png' },
+      { name: 'Чёрный неметаллик', hex: '#1a1a1a', img: 'assets/img/M6/black' },
+      { name: 'Серый неметаллик',  hex: '#c0c0c0', img: 'assets/img/M6/gray.png' },
+      { name: 'Белый неметаллик',  hex: '#f0f0f0', img: 'assets/img/M6/white.png' },
     ],
   },
   {
@@ -30,10 +30,10 @@ const cars = [
       'Допоборудование на 100 000р в подарок',
     ],
     colors: [
-      { name: 'Агат',             hex: '#b0b0b0', img: 'assets/img/cars/dargo-x/agate.png' },
-      { name: 'Чёрный нефрит',    hex: '#1a1a1a', img: 'assets/img/cars/dargo-x/black.png' },
-      { name: 'Графитовый кварц', hex: '#4a4a4a', img: 'assets/img/cars/dargo-x/graphite.png' },
-      { name: 'Терракотовый',     hex: '#c0522a', img: 'assets/img/cars/dargo-x/terracotta.png' },
+      { name: 'Агат',             hex: '#b0b0b0', img: 'assets/img/DRAGO-X/agat.png' },
+      { name: 'Чёрный нефрит',    hex: '#1a1a1a', img: 'assets/img/DRAGO-X/black-jade.png' },
+      { name: 'Графитовый кварц', hex: '#4a4a4a', img: 'assets/img/DRAGO-X/graphit-quartz.png' },
+      { name: 'Терракотовый',     hex: '#c0522a', img: 'assets/img/DRAGO-X/terrakota.png' },
     ],
   },
 ];
